eeb1c4b83bf12c58bc34360c3ad386b4febecd69
beepscore <support@beepscore.com>
2017-06-19 18:32:35 -0700

    Initial Commit

---
+++ CodablePlay/CodablePlay.h
@@ -0,0 +1,19 @@
+//
+//  CodablePlay.h
+//  CodablePlay
+//
+//  Created by Steve Baker on 6/19/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodablePlay.
+FOUNDATION_EXPORT double CodablePlayVersionNumber;
+
+//! Project version string for CodablePlay.
+FOUNDATION_EXPORT const unsigned char CodablePlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodablePlay/PublicHeader.h>
+
+
