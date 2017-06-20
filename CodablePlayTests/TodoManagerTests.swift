ca1bb63b7f41cbd4b024c57fb08bdcbb47b24c97
beepscore <support@beepscore.com>
2017-06-19 23:35:42 -0700

    In TodoManagerTests add testTodoByID

--- CodablePlayTests/TodoManagerTests.swift
+++ CodablePlayTests/TodoManagerTests.swift
@@ -14,5 +14,39 @@ class TodoManagerTests: XCTestCase {
     func testUrlForID() {
         XCTAssertEqual(TodoManager.endpointForID(5), "https://jsonplaceholder.typicode.com/todos/5")
     }
-    
+
+    /// Asynchronous test
+    /// https://developer.apple.com/documentation/xctest/asynchronous_tests_and_expectations/testing_asynchronous_operations_with_expectations
+    func testTodoByID() {
+
+        let expectation = XCTestExpectation(description: "download todo")
+
+        let completionHandler = { (todo: Todo?, error: Error?) -> Void in
+            if let error = error {
+                // got an error in getting the data, need to handle it
+                print(error)
+                return
+            }
+            guard let todo = todo else {
+                let failMessage = "error getting first todo: result is nil"
+                XCTFail(failMessage)
+                return
+            }
+            // success :)
+            debugPrint(todo)
+            XCTAssertNotNil(todo, "No todo was downloaded.")
+            XCTAssertEqual(todo.title, "laboriosam mollitia et enim quasi adipisci quia provident illum")
+            XCTAssertEqual(todo.id!, 5)
+            XCTAssertEqual(todo.userId, 1)
+            XCTAssertEqual(todo.completed, 0)
+
+            expectation.fulfill()
+        }
+
+        // call method under test
+        TodoManager.todoByID(5, completionHandler: completionHandler)
+
+        // wait until the expectation is fulfilled
+        wait(for: [expectation], timeout: 10.0)
+    }
 }
